e6b50edbff132b73b0f17e049888d55f061d4b71
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-18 19:51:33 -0400

    profile picture works

--- src/js/controllers/user-page.js
+++ src/js/controllers/user-page.js
@@ -4,28 +4,29 @@ $scope.photos = []
 
 function init () {
 
-  $http.get(`${SERVER}/user/${state.params.userid}/photos`).then(resp => {
+  $http.get(`${SERVER}/user/${$state.params.userid}/photos`).then(resp => {
     console.log(resp.data);
     $scope.user = resp.data;
-    $scope.photos= resp.data.photos;
+    $scope.photos = resp.data.photos;
     console.log($scope.photos);
 
   });
-
-}
+};
 
 init();
 
 
+
 $scope.addProfilepic = (info) => {
-  $http.post(`${SERVER}/update/${state.params.userid}`, info).then( resp => {
+  $http.put(`${SERVER}/update/${$state.params.userid}`, info).then( resp => {
     console.log(resp.data);
   })
-  //.then($state.go('/user'))
-  .cath(error => {
+  .then( $state.reload())
+  .catch(error => {
     console.log(error);
   });
 };
+
 };
 
 
